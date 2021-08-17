1a50af831aee8dd4d6c7ea15e2198b5dc2c43663
Vu Pham <vu0147369@gmail.com>
2021-08-17 13:28:00 +0700

    initial commit

---
+++ app/src/main/java/com/example/pavedeeplink/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.pavedeeplink
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    var count = 1
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show();
+            findViewById<TextView>(R.id.textview_first).text = count.toString()
+            count++
+        }
+
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        val appLinkAction: String? = intent?.action
+        val appLinkData: Uri? = intent?.data
+        showDeepLinkOffer(appLinkAction, appLinkData)
+    }
+
+    private fun showDeepLinkOffer(appLinkAction: String?, appLinkData: Uri?) {
+
+        Log.d(TAG, "showDeepLinkOffer:" +
+                "\n appLinkAction= $appLinkAction," +
+                " \n appLinkData= $appLinkData")
+        if (Intent.ACTION_VIEW == appLinkAction && appLinkData != null) {
+            val code = appLinkData.getQueryParameter("code")
+            if (code.isNullOrBlank().not()) {
+
+            } else {
+
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
